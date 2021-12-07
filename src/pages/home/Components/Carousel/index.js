430040819d26f06e769a8e613e399e294d3ef072
Doug <86627627+Dougsss@users.noreply.github.com>
2021-12-07 04:16:52 -0300

    tamanho carousel/img exemplo

--- src/pages/home/Components/Carousel/index.js
+++ src/pages/home/Components/Carousel/index.js
@@ -12,24 +12,49 @@ const Carrossel = () => {
         <Carousel variant="dark">
           <Carousel.Item>
             <Row>
-              <Col md={3} sm={6}>
+              <Col md={4} sm={6}>
                 <img
-                  className="d-block w-100"
-                  src=""
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/61hAn48vroL._AC_SL1500_.jpg"
                   alt="Second slide"
                 />
               </Col>
-              <Col md={3} sm={6} className="d-none d-sm-block">
+              <Col md={4} sm={6} className="d-none d-sm-block">
                 <img
-                  className="d-block w-100"
-                  src=""
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/61Khvv7MSGL._AC_SL1000_.jpg"
                   alt="Second slide"
                 />
               </Col>
-              <Col md={3} sm={6} className="d-none d-md-block">
+              <Col md={4} sm={6} className="d-none d-md-block">
                 <img
-                  className="d-block w-100"
-                  src=""
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/61IJBsHm97L._AC_SL1500_.jpg"
+                  alt="Second slide"
+                />
+              </Col>
+           </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row>
+              <Col md={4} sm={6}>
+                <img
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/613cWkz9GtS._AC_SL1107_.jpg"
+                  alt="Second slide"
+                />
+              </Col>
+              <Col md={4} sm={6} className="d-none d-sm-block">
+                <img
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/81JdWsw6h4L._AC_SL1500_.jpg"
+                  alt="Second slide"
+                />
+              </Col>
+              <Col md={4} sm={6} className="d-none d-md-block">
+                <img
+                  className="d-block w-100 h-75"
+                  src="https://m.media-amazon.com/images/I/81ugUo9f9PL._AC_SL1500_.jpg"
                   alt="Second slide"
                 />
               </Col>
