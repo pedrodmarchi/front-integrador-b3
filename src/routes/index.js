76268e84bc26e4dc5c9c927393c72b42a32e26e0
pedrodmarchi <78434099+pedrodmarchi@users.noreply.github.com>
2021-12-10 23:40:30 -0300

    asfa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from '../pages/Home';
+import Home from '../pages/Home/index';
 import Products from '../pages/Products';
 import Product from '../pages/Product';
 import Cart from '../pages/Cart';
